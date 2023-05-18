f1c8220265878b24b7779e178306d0e310733365
tognia <h.tognia@ziyouma-agency.com>
2023-05-18 18:59:21 +0100

    Getting a single competition with param not good

--- lambdas/getSingleCompetition.js
+++ lambdas/getSingleCompetition.js
@@ -1,29 +1,33 @@
-const AWS = require('aws-sdk');
-AWS.config.update({ region: 'us-east-1' });
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-module.exports.getItem = async (event, context) => { //pkParam, skParam
+module.exports.getItem = async (event, context) => {
   // let {param1, param2 } = event.pathParameters;
-  const { param1, param2 } = event.queryStringParameters;
-  console.log({param1}, {param2});
+  // const { param1, param2 } = event.queryStringParameters;
+  let param1 = event.pathParameters.param1;
+  let param2 = event.pathParameters.param2;
+  console.log(param1, param2);
   const params = {
-    TableName: 'ondebrief-dev', 
+    TableName: "ondebrief-dev",
     Key: {
-      PK: param1.toString(),
-      SK: param2.toString()
-    }
+      PK: param1,
+      SK: param2,
+    },
   };
   try {
     const result = await dynamoDB.get(params).promise();
     return {
       statusCode: 200,
-      body: JSON.stringify(result.Item)
+      body: JSON.stringify(result.Item),
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify(error)
+      body: JSON.stringify(error),
     };
   }
 };
+// PK: 'COMPETITION#0eb733be-3f65-4ec3-bdb4-5b6f58288309',
+// SK: 'COMPETITION#0eb733be-3f65-4ec3-bdb4-5b6f58288309'
