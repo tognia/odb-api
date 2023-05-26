64d944c38d4908781d3a05db1c774d872a3dbf2c
unknown <h.tognia@ziyouma-agency.com>
2023-05-26 15:37:43 +0100

    Add Lambda Authorizer

---
+++ authorizer/handler.js
@@ -0,0 +1,30 @@
+module.exports.authorizer = async(event) => {
+    console.log('event', event)
+
+    const token = event['authorizationToken']
+
+    console.log('token', token)
+    
+    
+    let permission = "Deny";
+    if(token === "my-secret-token") {
+    	permission = "Allow"
+    }
+    const authResponse = { 
+        "principalId": "abc123", 
+        "policyDocument": 
+            { 
+                "Version": "2012-10-17", 
+                "Statement": 
+                        [
+                            {
+                                "Action": "execute-api:Invoke", 
+                                "Resource": ["arn:aws:execute-api:us-east-1:150441806096:eemqoozgtl/dev/GET/getcompetitions"], 
+                                "Effect": `${permission}`
+                            }
+                        ]
+            }
+        
+    }
+    return authResponse;
+};
