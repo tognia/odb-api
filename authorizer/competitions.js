5e5cdf39ae47d64d529c2e10e5ed7319a8329f8f
unknown <h.tognia@ziyouma-agency.com>
2023-06-01 17:26:58 +0100

    Adding environmental args serverless framework almost good

--- authorizer/competitions.js
+++ authorizer/competitions.js
@@ -3,8 +3,8 @@ module.exports.authorizer = async(event) => {
 
     const token = event['authorizationToken'];
     const stage = process.env?.STAGE;
-    const apiId = process.env?.API_ID;
-     console.log("Current stage and apiId : ", stage, apiId);
+    // const apiId = process.env?.API_ID;
+    //  console.log("Current stage and apiId: ", stage);
 
     console.log('token', token);
 
@@ -14,7 +14,7 @@ module.exports.authorizer = async(event) => {
     	permission = "Allow"
     }
     const authResponse = { 
-        "principalId": "ymumz18hhl", 
+        "principalId": "abc123", 
         "policyDocument": 
             { 
                 "Version": "2012-10-17", 
@@ -23,8 +23,8 @@ module.exports.authorizer = async(event) => {
                             {
                                 "Action": "execute-api:Invoke", 
                                 "Resource": (stage!== undefined && stage.toString()==="v1")?[
-                                                                "arn:aws:execute-api:us-east-1:23x0jag7rg:".concat(apiId).concat("/v1/GET/competitions/*") ,
-                                                                "arn:aws:execute-api:us-east-1:23x0jag7rg:".concat(apiId).concat("/v1/GET/competitions")
+                                                                "arn:aws:execute-api:us-east-1:150441806096:23x0jag7rg/v1/GET/competitions/*",
+                                                                "arn:aws:execute-api:us-east-1:150441806096:23x0jag7rg/v1/GET/competitions"
                                                         ]:
                                                         [
                                                                 "arn:aws:execute-api:us-east-1:150441806096:ymumz18hhl/dev/GET/competitions/*",
