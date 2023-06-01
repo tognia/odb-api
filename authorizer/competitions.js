3acd8ba5af7cae1926cc6f2801b7e4d008847701
unknown <h.tognia@ziyouma-agency.com>
2023-06-01 16:51:20 +0100

    Adding environmental args serverless framework not good

--- authorizer/competitions.js
+++ authorizer/competitions.js
@@ -1,21 +1,20 @@
 module.exports.authorizer = async(event) => {
     console.log('event', event)
 
-    const token = event['authorizationToken']
+    const token = event['authorizationToken'];
+    const stage = process.env?.STAGE;
+    const apiId = process.env?.API_ID;
+     console.log("Current stage and apiId : ", stage, apiId);
 
     console.log('token', token);
 
-    // let id = event.pathParameters?.id;
-
-    // console.log('id 2023', id);
-    
-    
+  
     let permission = "Deny";
     if(token === "my-secret-token") {
     	permission = "Allow"
     }
     const authResponse = { 
-        "principalId": "abc123", 
+        "principalId": "ymumz18hhl", 
         "policyDocument": 
             { 
                 "Version": "2012-10-17", 
@@ -23,11 +22,15 @@ module.exports.authorizer = async(event) => {
                         [
                             {
                                 "Action": "execute-api:Invoke", 
-                                "Resource": [
-                                            //   `arn:aws:execute-api:us-east-1:150441806096:ymumz18hhl/dev/GET/competitions/${id}`,
-                                              "arn:aws:execute-api:us-east-1:150441806096:ymumz18hhl/dev/GET/competitions/*",
-                                              "arn:aws:execute-api:us-east-1:150441806096:ymumz18hhl/dev/GET/competitions"
-                                            ], 
+                                "Resource": (stage!== undefined && stage.toString()==="v1")?[
+                                                                "arn:aws:execute-api:us-east-1:23x0jag7rg:".concat(apiId).concat("/v1/GET/competitions/*") ,
+                                                                "arn:aws:execute-api:us-east-1:23x0jag7rg:".concat(apiId).concat("/v1/GET/competitions")
+                                                        ]:
+                                                        [
+                                                                "arn:aws:execute-api:us-east-1:150441806096:ymumz18hhl/dev/GET/competitions/*",
+                                                                "arn:aws:execute-api:us-east-1:150441806096:ymumz18hhl/dev/GET/competitions"
+                                                        ]
+                                            , 
                                 "Effect": `${permission}`
                             }
                         ]
